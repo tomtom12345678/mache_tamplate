5f59bf0037c340a749d56978292eb83ec9078f5f
unknown <thomaszlotnik@gmail.com>
2018-08-24 17:35:14 +0200

    change background function

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@ let nav = document.querySelector('.hamburger');
 nav.addEventListener('click', function(){
     document.querySelector('.page-header').classList.toggle('nav-opened');
 })
+
+
+
+function changeStyle(e) {
+  const element = e.target;
+  if(element.classList.contains('btn')) {
+    element.parentElement.parentElement.querySelector('.plate-top').classList.toggle('bg-pink');
+  }
+}
+
+const cards = document.querySelector('.cards');
+cards.addEventListener('mouseover', (e) => { changeStyle(e); });
+cards.addEventListener('mouseout', (e) => { changeStyle(e); });
